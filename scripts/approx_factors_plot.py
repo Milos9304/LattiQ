68f05e03b3d31e179579ec56442894ca970c7def
Your Name <you@example.com>
2024-09-17 11:50:52 +0200

    .

--- scripts/approx_factors_plot.py
+++ scripts/approx_factors_plot.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
 
-import sys
+def f(x, a, b):
+    return a*(x**b)
 
-sys.path[0]=''
-
-import maplotlib.pyplot as plt
+ms=[4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 
 #CM_QAOA
 a=[1.00001,1.15984,2.28085,1.60549,2.1921,5.93133,9.88554,13.8006,27.4429,56.3166,90.6087,159.448,202.084,243.47,293.127,325.474,402.876,441.893,492.594,]
@@ -12,6 +13,26 @@ a=[1.00001,1.15984,2.28085,1.60549,2.1921,5.93133,9.88554,13.8006,27.4429,56.316
 #QAOA
 a2=[1.14711,1.18816,1.13386,1.68751,2.77557,2.99467,4.79571,7.40275,93.451,179.464,256.138,355.215,320.873,358.389,389.017,451.603,606.609,597.354,679.494,]
 
-plt.plot(a)
-plt.plot(a2)
+popt, pcov = curve_fit(f, ms, a, maxfev=30000)
+print("popt: ", popt)
+print("pcov: ", pcov)
+
+popt2, pcov2 = curve_fit(f, ms, a2, maxfev=30000)
+print("popt2: ", popt2)
+print("pcov2: ", pcov2)
+
+
+plt.plot(ms, a, label="CM_QAOA approx factor")
+plt.plot(ms, a2, label="QAOA approx factor")
+
+plt.plot(ms, f(ms, *popt), label="CM_QAOA fit "+str(round(popt[0], 2))+"*x^"+str(round(popt[1], 2)))
+plt.plot(ms, f(ms, *popt2), label="QAOA fit "+str(round(popt2[0], 2))+"*x^"+str(round(popt2[1], 2)))
+
+
+
+plt.xticks(ms)
+plt.title("approx_factors_plot.py")
+plt.xlabel("lattice dimension")
+plt.ylabel("approximation factor")
+plt.legend()
 plt.show()
