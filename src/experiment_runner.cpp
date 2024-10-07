c0d6f1e4fcad8df60433a98164846960dcaf3bda
milos <prokop.milos@gmail.com>
2024-10-07 19:04:47 +0100

    .

--- src/experiment_runner.cpp
+++ src/experiment_runner.cpp
@@ -665,7 +665,7 @@ void AngleResultsExperiment::run(){
 
 	this->mapOptions->penalty = 0;
 
-	for(int index = 0; index < 2; ++index){
+	for(int index = 0; index < 1; ++index){
 
 		if(index == 0){
 			std::cerr<<std::endl<<"  CM-QAOA"<<std::endl<<std::endl;
@@ -1099,7 +1099,7 @@ AngleExperimentBase::Cost AngleExperimentBase::_cost_fn(std::vector<Instance>* d
 			for(long long int j = 1; j < buffer.stateVector->numAmpsTotal; ++j){
 
 					long long int index = refEnergies[j].index;
-					if(refEnergies[j].value <= refEnergies[1].value * pow(instance.h.nbQubits, 2)){
+					if(refEnergies[j].value <= refEnergies[1].value * pow(instance.h.nbQubits, 4)){
 						overlapp += buffer.stateVector->stateVec.real[index]*buffer.stateVector->stateVec.real[index]+buffer.stateVector->stateVec.imag[index]*buffer.stateVector->stateVec.imag[index];
 						nsols++;
 					}
