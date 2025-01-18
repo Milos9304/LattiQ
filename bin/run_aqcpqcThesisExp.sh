9d733d6324b511642975095f474949400b673f30
Your Name <you@example.com>
2025-01-18 15:06:13 +0100

    .

--- bin/run_aqcpqcThesisExp.sh
+++ bin/run_aqcpqcThesisExp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for i in {5..20}; do
-    ./averagedSvp -q 1 --aqcpqc --mstart 4 --mend 4 --steps "$i" --aqcpqcdepth 1 --inst 2
+#for i in {2..20}; do
+#    ./averagedSvp -q 1 --aqcpqc --mstart 4 --mend 4 --steps "$i" --aqcpqcdepth 1 --inst 10 
+#done
+
+for i in {2..20}; do
+    ./averagedSvp -q 1 --aqcpqc --mstart 4 --mend 4 --steps "$i" --aqcdepth 2 --inst 10
 done
