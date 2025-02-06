f7877fc656cab71ba2671135540d76224ac0a1bd
milos <prokop.milos@gmail.com>
2025-02-06 08:40:16 +0000

    .

--- fixed_scripts_4dim_1depth/fixed_run_aqcpqcThesisExp.sh
+++ fixed_scripts_4dim_1depth/fixed_run_aqcpqcThesisExp.sh
@@ -4,6 +4,6 @@
 #    ../bin/averagedSvp -q 1 --aqcpqc --mstart 3 --mend 3 --steps "$i" --aqcdepth 2 --inst 10 -l 0
 #done
 
-for i in {2..50}; do
+for i in {15..50}; do
     ../bin/averagedSvp -q 1 --aqcpqc --mstart 4 --mend 4 --steps "$i" --aqcdepth 1 --inst 10 -l 0
 done
