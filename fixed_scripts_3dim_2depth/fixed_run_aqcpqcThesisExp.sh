7cabb653bc2371033a764a68719daf8537467d9e
Your Name <you@example.com>
2025-02-06 16:32:36 +0100

    .

--- fixed_scripts_3dim_2depth/fixed_run_aqcpqcThesisExp.sh
+++ fixed_scripts_3dim_2depth/fixed_run_aqcpqcThesisExp.sh
@@ -4,6 +4,8 @@
 #    ../bin/averagedSvp -q 1 --aqcpqc --mstart 3 --mend 3 --steps "$i" --aqcdepth 2 --inst 10 -l 0
 #done
 
-for i in {2..50}; do
-    ../bin/averagedSvp -q 1 --aqcpqc --mstart 3 --mend 3 --steps "$i" --aqcdepth 2 --inst 10 -l 0
-done
+#for i in {2..50}; do
+#    ../bin/averagedSvp -q 1 --aqcpqc --mstart 3 --mend 3 --steps "$i" --aqcdepth 2 --inst 10 -l 0
+#done
+
+seq 46 50 | parallel -j4 --lb "CORE=\$((({}-2)%4)); echo Running {} on \$CORE; taskset -c \$CORE ../bin/averagedSvp -q 1 --aqcpqc --mstart 3 --mend 3 --steps {} --aqcdepth 2 --inst 10 -l 0 > /dev/null 2> /dev/null"
